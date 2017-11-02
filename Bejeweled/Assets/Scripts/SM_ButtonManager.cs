e76448d2db81544b9211e4607e0203725c3ca5d0
SemiyonX2/Shane Meitzner <shanemeitzner@yahoo.com>
2017-11-01 20:03:44 -0400

    Initial commit

---
+++ Bejeweled/Assets/Scripts/SM_ButtonManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SM_ButtonManager : MonoBehaviour{
+
+	//Author: Shane Meitzner
+	//Date: 10/30/2017
+	
+	public Transform canvas;
+
+	public void StartGameButton (string loadLevel){
+		SceneManager.LoadScene (loadLevel);
+	}
+	public void QuitGameButton (string quitGame){
+		Application.Quit ();
+	}
+	public void CreditsGameButton (string showCredits){
+		SceneManager.LoadScene (showCredits);
+	}
+	public void MainMenuButton (string mainMenu){
+		SceneManager.LoadScene (mainMenu);
+	}
+    public void ControlsButton (string showControls) {
+        SceneManager.LoadScene (showControls);
+    }
+	public void PauseMenu (){
+		if (canvas.gameObject.activeInHierarchy == false)
+		{
+			canvas.gameObject.SetActive(true);
+			Time.timeScale = 0;
+		}
+		else
+		{
+			canvas.gameObject.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+
+}
+
---
+++ Bejeweled/Assets/Scripts/SM_PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SM_PauseMenu : MonoBehaviour {
+	
+	//Author: Shane Meitzner
+	//Date: 10/30/2017
+
+    public Transform canvas;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       // if (Input.GetKeyDown(KeyCode.Escape))
+       // {
+            //if (canvas.gameObject.activeInHierarchy == false)
+           // {
+         //       canvas.gameObject.SetActive(true);
+         //       Time.timeScale = 0;
+         //   }
+         //   else
+         //   {
+         //       canvas.gameObject.SetActive(false);
+         //       Time.timeScale = 1;
+         //   }
+       // }
+	
+	}
+	public void PauseMenu (){
+		if (canvas.gameObject.activeInHierarchy == false)
+		{
+			canvas.gameObject.SetActive(true);
+			Time.timeScale = 0;
+		}
+		else
+		{
+			canvas.gameObject.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+}
