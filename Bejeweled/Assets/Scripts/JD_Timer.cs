8649164408e477ad90fb5d9c6c83342c0e452119
JacobDunbar <bigjake31713@gmail.com>
2017-11-04 16:55:20 -0400

    Timer
    Remade the timer because GItHub deleted it

---
+++ Bejeweled/Assets/Scripts/JD_Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JD_Timer : MonoBehaviour {
+
+    float timeLeft = 60.0f;
+
+    public Text text;
+
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = " " + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
