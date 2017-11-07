fb9f2cc088057216b07f9a733635d25012d653fd
JacobDunbar <bigjake31713@gmail.com>
2017-11-06 19:57:56 -0500

    Scoring
    reupload scoring

--- Bejeweled/Assets/Scripts/JD_Leaderboard.cs
+++ Bejeweled/Assets/Scripts/JD_Leaderboard.cs
@@ -1,17 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JD_Leaderboard : MonoBehaviour {
 
+    //Author: Jacob Dunbar
+    //Date: 10/31/17
 
-    
-	// Use this for initialization
-	void Start () {
-        
-	}
+    int score;
+    int highScore;
+    public Text text;
+    public Text highscoreText;
+    public int[] highscores = new int[10];
+    string username;
+    public bool scored;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        PlayerPrefs.SetString("username", username);
+        text = GetComponent<Text>();
+        score = 0;
+        highScore = PlayerPrefs.GetInt("highscore");
+    }
+
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+        Score();
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("highscore", highScore);
+            highscoreText.text = "Highscore:" + highScore;
+        }
+        text.text = "" + score;
+        // displays highscore 
+        highscoreText.text = "" + highScore;
+
+    }
+
+    void Score()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            score++;
+            scored = true;
+        }
+        else
+        {
+            scored = false;
+        }
+    }
 }
