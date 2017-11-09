3d1483ea34cf2bd133229c88198e6df602645ab3
SemiyonX2/Shane Meitzner <shanemeitzner@yahoo.com>
2017-11-09 14:42:31 -0500

    Adding ui everything
    yaaaay

---
+++ Bejeweled/Assets/Scripts/SM_BackgroundChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SM_BackgroundChange : MonoBehaviour {
+	[SerializeField]private Camera camera;
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent <Camera> ();
+		camera.backgroundColor = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Bejeweled/Assets/Scripts/SM_PauseMenu.cs
+++ Bejeweled/Assets/Scripts/SM_PauseMenu.cs
@@ -37,12 +37,12 @@ public class SM_PauseMenu : MonoBehaviour {
 		if (canvas.gameObject.activeInHierarchy == false)
 		{
 			canvas.gameObject.SetActive(true);
-			Time.timeScale = 0;
+			//Time.timeScale = 0;
 		}
 		else
 		{
 			canvas.gameObject.SetActive(false);
-			Time.timeScale = 1;
+			//Time.timeScale = 1;
 		}
 	}
 }
